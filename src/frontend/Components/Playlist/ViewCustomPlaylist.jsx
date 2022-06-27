82d307e136de0be04355569f4a0bcec7f5d4815c
Varu <vardaan.agarwal.98@gmail.com>
2022-06-27 10:34:28 +0530

    merge conflict: add playlist container map

---
+++ src/frontend/Components/Playlist/ViewCustomPlaylist.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ViewCustomPlaylist.css";
+
+const ViewCustomPlaylist = () => {
+  return (
+    <div>The user defined single selected playlist will be shown here</div>
+  );
+};
+
+export default ViewCustomPlaylist;
