74f649c5b84cff4be11131ee2dbd8437e9692c4a
Varu <vardaan.agarwal.98@gmail.com>
2022-07-15 01:26:14 +0530

    chore : remove unused import

--- src/frontend/Components/Playlist/NewPlaylistModal.jsx
+++ src/frontend/Components/Playlist/NewPlaylistModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { usePlaylist } from "../../Context/PlaylistContext";
 import { useAuth } from "../../Context/AuthContext";
 import "./NewPlaylistModal.css";
